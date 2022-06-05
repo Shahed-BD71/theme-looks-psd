56af9c868309682baf6ef053cff360521af0e7c3
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-06-05 23:55:20 +0600

    why-choose-us done

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// <img src="./images/brash.png" alt="">
+// <img src="./images/code.png" alt="">
+// <img src="./images/SEO.png" alt="">
+// <img src="./images/android.png" alt="">
+// <img src="./images/image.png" alt=""></img>
