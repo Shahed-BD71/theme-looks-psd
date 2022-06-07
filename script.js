b9eeedb50bcf732f8608c26aad3410da1402247a
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-06-07 12:40:16 +0600

    hero-done

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// <img src="./images/brash.png" alt="">
-// <img src="./images/code.png" alt="">
-// <img src="./images/SEO.png" alt="">
-// <img src="./images/android.png" alt="">
-// <img src="./images/image.png" alt=""></img>
